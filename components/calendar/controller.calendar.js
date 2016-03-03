d8b7fb590dda24bbd7e9019235181e37740ab978
Justin McBride <justin@dare599z.com>
2016-03-02 22:32:30 -0700

    change view of home state and calendar

--- components/calendar/controller.calendar.js
+++ components/calendar/controller.calendar.js
@@ -1,12 +1,7 @@
 'use strict';
 
 calendar.controller( "Controller_Calendar", [ '$scope', function( $scope ) {
-  $scope.name = "my calendar";
-  $scope.month = "November";
-  $scope.day = 11;
-
   $scope.calendarView = 'month';
   $scope.calendarDate = moment();
   $scope.isCellOpen = true;
-
 }]);
--- components/event/controller.event.js
+++ components/event/controller.event.js
@@ -1,8 +1,7 @@
 'use strict';
 
 calendar.controller( "Controller_Event", ['$scope', '$http', '$stateParams', function( $scope, $http, $stateParams ) {
-  var id_event = $stateParams.id_event;
-  $http.get( 'http://localhost:3111/api/v1/events/' + id_event )
+  $http.get( 'http://localhost:3111/api/v1/events/' + $stateParams.id_event )
     .success( function(response) {
       $scope.input_title = response.Title;
       $scope.input_cost = response.Cost;
@@ -14,5 +13,4 @@ calendar.controller( "Controller_Event", ['$scope', '$http', '$stateParams', fun
       console.log( "error" );
     })
   ;
-
 }]);
