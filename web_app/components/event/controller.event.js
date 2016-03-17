3be4a84a1fb47772f90e85f66c7063d8b00add4f
Justin McBride <justin@dare599z.com>
2016-03-17 14:52:40 -0600

    upgrade a lot to use mongodb

--- web_app/components/event/controller.event.js
+++ web_app/components/event/controller.event.js
@@ -1,28 +1,66 @@
 'use strict';
 
 calendar.controller( "Controller_Event", ['$scope', '$http', '$stateParams', function( $scope, $http, $stateParams ) {
-  $http.get( 'http://localhost:3111/api/v1/events/' + $stateParams.id_event )
+  $scope.id = $stateParams.id_event;
+  $scope.Categories = ['Art', 'Entertainment', 'Dog Shows', 'Athletic Competitions'];
+  $scope.input_category = $scope.Categories[0];
+  $scope.input_privacy = "Public";
+
+  if( $scope.id )
+  {
+    $http.get( 'http://localhost:3111/api/v1/events/' + $stateParams.id_event )
+      .success( function(response) {
+        $scope.input_title = response.Title;
+        $scope.input_cost = response.Cost;
+        $scope.input_category = response.Category;
+        $scope.input_location = response.Location;
+        $scope.input_description = response.Description;
+      })
+      .error( function() {
+        console.log( "error getting event" );
+      })
+    ;
+  }
+
+  $http.get( 'http://localhost:3111/api/v1/groups/56dd2c3aa0a5453d0baa35e9' )
     .success( function(response) {
-      $scope.input_title = response.Title;
-      $scope.input_cost = response.Cost;
-      $scope.input_category = response.Category;
-      $scope.input_location = response.Location;
-      $scope.input_description = response.Description;
-    })
-    .error( function() {
-      console.log( "error" );
-    })
-  ;
-  $http.get( 'http://localhost:3111/api/v1/groups/' )
-    .success( function(response) {
+      console.log( response );
       $scope.Groups = response;
     })
     .error( function() {
-      console.log( "error" );
+      console.log( "error getting groups" );
     })
   ;
+
+  $scope.onClick_save = function() {
+    console.log( "clicked save" );
+    var details = {
+      title: $scope.input_title,
+      cost: $scope.input_cost,
+      location: $scope.input_location,
+      description: $scope.input_description,
+      date: $scope.input_date,
+      time_start: $scope.input_time_start,
+      time_end: $scope.input_time_end,
+      category: $scope.input_category,
+      privacy: $scope.input_privacy
+    };
+    $http.post( 'http://localhost:3111/api/v1/events', details )
+      .success( function( response ) {
+        console.log( "cool" );
+      })
+      .error( function() {
+        console.log( "couldn't create new event" );
+      });
+  };
+
   $scope.date_options = {
     showWeeks: false,
   };
-  $scope.hours_24 = false;
+
+  $scope.hours_24 = true;
+  $scope.hstep = 1;
+  $scope.mstep = 15;
+  $scope.input_time_start = moment();
+  $scope.input_time_end = moment();
 }]);
--- web_app/components/groups/controller.group.meta.js
+++ web_app/components/groups/controller.group.meta.js
@@ -1,7 +1,7 @@
 'use strict';
 
 calendar.controller( "Controller_Group_Meta", function( $scope, $state, $http, $stateParams, $view ) {
-  $http.get( 'http://localhost:3111/api/v1/groups/' )
+  $http.get( 'http://localhost:3111/api/v1/groups/56dd2c3aa0a5453d0baa35e9' )
     .success( function(response) {
       $scope.Groups = response;
     })
@@ -13,5 +13,5 @@ calendar.controller( "Controller_Group_Meta", function( $scope, $state, $http, $
   $scope.event_clicked_groupName = function( id ) {
     console.log( "going to group with id=" + id );
     $state.go( "group.id", { id_group: id } );
-  }
+  };
 });
