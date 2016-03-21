c9e5f43fd805660e79de936f1abc6f74f12d4fb2
Justin McBride <justin@dare599z.com>
2016-03-21 00:21:20 -0600

    update web app to add events etc

--- web_app/components/calendar/controller.calendar.js
+++ web_app/components/calendar/controller.calendar.js
@@ -4,11 +4,4 @@ calendar.controller( "Controller_Calendar", [ '$scope', '$uibModal', function( $
   $scope.calendarView = 'month';
   $scope.calendarDate = moment();
   $scope.isCellOpen = true;
-  $scope.onClick_createEvent = function() {
-    var modal = $uibModal.open({
-      animation: true,
-      templateUrl: 'components/event/edit_event.html',
-      controller: 'Controller_Event'
-    })
-  }
 }]);
