d6029c26c708075c9b75a48d8b22c7ee771ec53b
Justin McBride <justin@dare599z.com>
2016-03-21 00:20:13 -0600

    update rest server to includer basic adding of users and events

--- api_server/node-server.js
+++ api_server/node-server.js
@@ -67,8 +67,14 @@ server.get( '/api/v1/groups/:userID', function(req, res) {
         console.log( error );
         return;
       }
-      console.log( "groups result: " + user.groups );
-      res.json( user.groups );
+      if(user && user.groups) {
+        console.log( "groups result: " + user.groups );
+        res.json( user.groups );
+      }
+      else {
+        res.send()
+      }
+
     });
   // models.User.find( { members: userID }, function( error, result ) {
   //   console.log( "groups result: " + result );
@@ -76,10 +82,115 @@ server.get( '/api/v1/groups/:userID', function(req, res) {
   // } );
 });
 
+server.post( '/api/v1/users', function(req, res) {
+  newUser = new models.User();
+  newUser.name = req.body.name;
+  newUser.email = req.body.email;
+  newUser.password = req.body.password;
+
+  newUser.save( function( error ) {
+    if( error ) {
+      console.log( "error saving new user: " + error );
+      res.send( 500, { status: 500, message: "couldn't create db entry: " + error, type: "internal" } );
+      return;
+    }
+
+    theirCalendar = new models.Calendar();
+    theirCalendar.name = newUser.name +"'s Personal";
+    theirCalendar.owner = newUser._id;
+
+
+    theirCalendar.save( function( error2 ) {
+      if( error2 ) {
+        console.log( "error saving new user's calendar: " + error2 );
+        res.send( 500, error2 );
+        return;
+      };
+      newUser.calendar = theirCalendar._id;
+      newUser.save( function( error3 ) {
+        res.json( { newId: newUser._id, calendar: newUser.calendar } );
+      });
+    });
+  });
+});
+
 server.post( '/api/v1/events', function(req, res) {
- var details = req.body;
- console.log( details );
- res.send();
+  var userID = req.body.userID;
+  var groupID = "???";
+
+  console.log( userID );
+
+
+  newEvent = new models.Event();
+  newEvent.title = req.body.title;
+  newEvent.cost = req.body.cost;
+  newEvent.location = req.body.location;
+  newEvent.description = req.body.description;
+  newEvent.date = req.body.date;
+  newEvent.category = req.body.category;
+
+  var saveToPersonalCalendar = function(eventID, userID) {
+    console.log( userID );
+    models.User
+      .findOne()
+      .where( { _id: userID } )
+      // .select( 'calendar' )
+      .populate( 'calendar' )
+      .exec( function( error, user ) {
+        if( error ) {
+          console.log( "error finding personal calendar" );
+          return false;
+        }
+        console.log( user );
+        if( !user ) {
+          return false;
+        }
+        user.calendar.events += eventID;
+        user.calendar.save(function( error2 ) {
+          if( error2 ) {
+            console.log( "error2" );
+            return false;
+          }
+          return true;
+        });
+      })
+    ;
+  }
+
+  newEvent.save( function(error) {
+    if( error ) {
+      console.log( "error saving new event: " + error );
+      res.send( 500, { status: 500, message: "couldn't create db entry: " + error, type: "internal" } );
+      return;
+    }
+
+    if( req.body.privacy === "Public" ) {
+      // post to personal calendar, and maybe some public one???
+      saveToPersonalCalendar( newEvent._id, userID );
+    }
+    else if( req.body.privacy === "Private" ) {
+      // only post to personal calendar
+      saveToPersonalCalendar( newEvent._id, userID );
+    }
+    else {
+      // post to personal and specific group
+      saveToPersonalCalendar( newEvent._id, userID );
+
+      models.Group
+        .findByIdAndUpdate( groupID, { $push: { events: newEvent._id } },
+          function( error, result ) {
+            if( error ) {
+              console.log( "error adding saved event to calendar: " + event );
+              res.send( 500, { status: 500, message: "couldn't create db entry: " + error, type: "internal" } );
+              return;
+            }
+          }
+        )
+      ; // end post to group calendar
+    }
+
+    res.send();
+  })
 });
 
 var server_port = 3111;
