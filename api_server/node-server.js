d756a0c28b311a00b04b405e21e2f6e1d7f2d19a
Justin McBride <justin@dare599z.com>
2016-03-23 17:06:52 -0600

    many documentation and logging changes to the rest api server

--- api_server/node-server.js
+++ api_server/node-server.js
@@ -1,10 +1,23 @@
-var express = require('express');
-var cors = require('cors');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var credentials = require('./credentials');
-var models = require('./db_schema');
+/*
+  This is the backend server logic that will run in one place when the application is ready to be deployed.
+  It contains API endpoints that the frontend AnguarJS application will communicate with in order
+  to fetch, store, and modify data within our application, but this backend application
+  allows us more control over what happens to the database. Our database is only accessible through
+  this api, and thus we must be gate-keepers here where we validate information going into the database.
+ */
 
+// import all of the required dependencies. the following lines will crash if you have not yet
+// run the `npm install` command
+var express = require('express'); // creates for the rest api
+var cors = require('cors'); // cross-origin request sharing allows sending data to foreign services
+var bodyParser = require('body-parser'); // parses all express body requests and allows easy access to the request parameters
+var mongoose = require('mongoose'); // facilitates connection to the MongoDB backend
+var credentials = require('./credentials'); // local file with the MongoDB credentials. separate file so that they're not accidently commited
+var models = require('./db_schema'); // representations of the DB for mongoose to communicate with mongo
+
+// do the actual connecting to the mongo database. currently it's stored on mongolabs
+// and you need the credentials that are shared only internally. modify the ./credentials.js
+// file with them so that mongoose can connect to mongo
 var options = {
   user: credentials.mongo_user,
   pass: credentials.mongo_pass
@@ -16,6 +29,9 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log( "Connected to mongo" );
 });
+
+// REST api implementation is below
+// this can probably be exported to a routes.js file
 
 var server = express();
 server.use( cors() );
@@ -36,50 +52,62 @@ server.get( '/api/v1/events/:id', function(req, res) {
 
 
 server.get( '/api/v1/group/:id', function(req, res) {
+  // this endpoint will return the information for a specific group,
+  // including a list of all the members and their information
   var groupID = req.params['id'];
   models.Group
     .findOne( { _id: groupID } )
-    .populate( 'members' )
+    .populate({
+      path: 'members', // expand the list of members
+      select: 'name _id' // but only include their name's and id's
+    })
     .exec( function( error, result ) {
       if( error ) {
         console.log( "group id error" );
         return;
       }
-      if( !result ) {
-        console.log( "group id not found" );
-        return;
+      else {
+        if( result ) {
+          console.log( result );
+          res.json( result );
+        }
+        else {
+          console.log( "group id not found" );
+          res.send( 500 );
+          return;
+        }
       }
-      console.log( result );
-      res.json( result );
     });
 });
 
 server.get( '/api/v1/groups/:userID', function(req, res) {
-  var userID = req.params['userID'];
+  // this endpoint should return a list of all the groups that a user belongs to
+  var userID = req.body.userID;
+
   models.User
     .findOne( { _id : userID } )
     .populate({
-      path: 'groups',
-      select: 'name _id'
+      path: 'groups', // expand out all of the groups
+      select: 'name _id' // but we only need the id and name
     })
     .exec( function( error, user ) {
       if( error ) {
-        console.log( error );
-        return;
+        console.log( "error finding user with id=" + userID + ", " + error );
+        res.send( 500 );
       }
-      if(user && user.groups) {
-        console.log( "groups result: " + user.groups );
-        res.json( user.groups );
+      else
+      {
+        if(user && user.groups) {
+          console.log( "groups result: " );
+          console.log( user.groups );
+          res.json( user.groups );
+        }
+        else {
+          res.send( 500 );
+        }
       }
-      else {
-        res.send()
-      }
-
-    });
-  // models.User.find( { members: userID }, function( error, result ) {
-  //   console.log( "groups result: " + result );
-  //   res.json( result );
-  // } );
+    }
+  );
 });
 
 server.post( '/api/v1/users', function(req, res) {
@@ -115,12 +143,17 @@ server.post( '/api/v1/users', function(req, res) {
 });
 
 server.post( '/api/v1/events', function(req, res) {
+  // when posting to this url, we need to create an event on the db,
+  // then add it to the appropriate places. it should always be put onto
+  // the creating user's personal calendar, as well as any selected
+  // calendars, such as a group
+  // we get the userID and other details from the post body
   var userID = req.body.userID;
   var groupID = "???";
 
-  console.log( userID );
+  console.log( "userID: " + userID );
 
-
+  // first, we create the document with the appropriate values
   newEvent = new models.Event();
   newEvent.title = req.body.title;
   newEvent.cost = req.body.cost;
@@ -129,42 +162,51 @@ server.post( '/api/v1/events', function(req, res) {
   newEvent.date = req.body.date;
   newEvent.category = req.body.category;
 
+  // define the below function to avoid putting it into three places
   var saveToPersonalCalendar = function(eventID, userID) {
-    console.log( userID );
+    console.log( "saveToPersonalCalendar: userID=" + userID );
     models.User
       .findOne()
       .where( { _id: userID } )
-      // .select( 'calendar' )
-      .populate( {
-        path: 'calendar',
-        select: 'name _id'
-      } )
+      .populate({
+        path: 'calendar', // expand out the calendar reference
+        select: 'name _id' // but only display the name and id
+      })
       .exec( function( error, user ) {
-        if( error ) {
-          console.log( "error finding personal calendar" );
+        if( error ){
+          console.log( "saveToPersonalCalendar: error retrieving user" );
           return false;
         }
         console.log( user );
         if( !user ) {
+          console.log( "saveToPersonalCalendar: no matching user found" );
           return false;
         }
-        var thing = models.Calendar.update(
+        models.Calendar.update(
           { _id: user.calendar._id },
           { $push: {
               events: eventID
             }
-          }, function( error2, result ) {
+          },
+          function( error2, result ) {
             if( error2 ) {
-              consoloe.log( "error saving event to calendar: " + error2 );
-              return;
+              consoloe.log( "saveToPersonalCalendar: error saving event to calendar: " + error2 );
+              return false;
             }
-            console.log( "event saved to personal calendar: " + result );
+            console.log( "event saved to personal calendar: " );
+            console.log( result );
+            return true;
           }
         );
       })
     ;
   }
 
+  // now we commit it to the database
+  // afater saving it, we will add it to the user's personal calendar,
+  // and we need to ensure that it happened properly.
+  // if not, return an error to the app
+  var savedToCalendarCorrectly = false;
   newEvent.save( function(error) {
     if( error ) {
       console.log( "error saving new event: " + error );
@@ -174,21 +216,21 @@ server.post( '/api/v1/events', function(req, res) {
 
     if( req.body.privacy === "Public" ) {
       // post to personal calendar, and maybe some public one???
-      saveToPersonalCalendar( newEvent._id, userID );
+      savedToCalendarCorrectly = saveToPersonalCalendar( newEvent._id, userID );
     }
     else if( req.body.privacy === "Private" ) {
       // only post to personal calendar
-      saveToPersonalCalendar( newEvent._id, userID );
+      savedToCalendarCorrectly = saveToPersonalCalendar( newEvent._id, userID );
     }
     else {
       // post to personal and specific group
-      saveToPersonalCalendar( newEvent._id, userID );
+      savedToCalendarCorrectly = saveToPersonalCalendar( newEvent._id, userID );
 
       models.Group
         .findByIdAndUpdate( groupID, { $push: { events: newEvent._id } },
           function( error, result ) {
             if( error ) {
-              console.log( "error adding saved event to calendar: " + event );
+              console.log( "error adding saved event to calendar: " + error );
               res.send( 500, { status: 500, message: "couldn't create db entry: " + error, type: "internal" } );
               return;
             }
@@ -201,7 +243,8 @@ server.post( '/api/v1/events', function(req, res) {
   })
 });
 
+// now that the endpoints have been created, start the server
 var server_port = 3111;
 server.listen( server_port, function() {
-  console.log( "Running server on " + server_port );
+  console.log( "Running backend nodejs server on port " + server_port );
 });
