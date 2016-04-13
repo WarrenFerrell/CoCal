d5d39df614dd5da0f54a8b084eab0e7be7667d07
Phantom1771 <justin.schiller@colorado.edu>
2016-04-13 00:10:40 -0600

    Updated notifications to reflect pst revert state

--- web_app/components/notifications/controller.notifications.js
+++ web_app/components/notifications/controller.notifications.js
@@ -1,41 +1,13 @@
 'use strict';
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 calendar.controller( "Controller_Notifications", function( $scope, $state, $http, $stateParams, $view, Session ) {
 	console.log("user NOtifications " + Session.id_user);
   $http.get( 'http://localhost:3111/api/v1/notifications/' + Session.id_user )
-
-=======
-calendar.controller( "Controller_Notifications", ['$scope', '$http', '$stateParams', 'Session', function( $scope, $http, $stateParams, Session ) {
-  $scope.user = "Test";
-  $scope.notifications = "THESE ARE NOTIFICATIONS";
-  $scope.cleared = "THESE USED TO BE NOTIFICATIONS";
-
-  $http.get( 'http://localhost:3111/api/v1/groups/' + Session.id_user )
->>>>>>> parent of 7fcd972... Updated notifications stuff
-=======
-calendar.controller( "Controller_Notifications", ['$scope', '$http', '$stateParams', 'Session', function( $scope, $http, $stateParams, Session ) {
-  $scope.user = "Test";
-  $scope.notifications = "THESE ARE NOTIFICATIONS";
-  $scope.cleared = "THESE USED TO BE NOTIFICATIONS";
-
-  $http.get( 'http://localhost:3111/api/v1/groups/' + Session.id_user )
->>>>>>> parent of 7fcd972... Updated notifications stuff
     .success( function(response) {
       console.log( response );
       $scope.Groups = response;
     })
     .error( function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
       console.log( "error getting notifications" );
-=======
-      console.log( "error getting groups" );
->>>>>>> parent of 7fcd972... Updated notifications stuff
-=======
-      console.log( "error getting groups" );
->>>>>>> parent of 7fcd972... Updated notifications stuff
     })
   ;
 
