e78838b037c650dd49a8fabc68a6cfe1ee8ed523
Phantom1771 <justin.schiller@colorado.edu>
2016-04-12 23:58:38 -0600

    hopefully fixed bad commit

--- web_app/components/notifications/controller.notifications.js
+++ web_app/components/notifications/controller.notifications.js
@@ -1,14 +1,17 @@
 'use strict';
+
 calendar.controller( "Controller_Notifications", function( $scope, $state, $http, $stateParams, $view, Session ) {
 	console.log("user NOtifications " + Session.id_user);
   $http.get( 'http://localhost:3111/api/v1/notifications/' + Session.id_user )
+
     .success( function(response) {
       console.log( response );
       $scope.notifications = response;
     })
     .error( function() {
       console.log( "error getting notifications" );
-    });
+    })
+  ;
 
   $scope.onClick_save = function() {
     console.log( "clicked save" );
