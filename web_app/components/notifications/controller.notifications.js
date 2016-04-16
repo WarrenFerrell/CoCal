a74e7a458ee987f6919df43707c9635034eb9740
Phantom1771 <justin.schiller@colorado.edu>
2016-04-16 15:10:58 -0600

    display notifications and add button to clear them

--- web_app/components/notifications/controller.notifications.js
+++ web_app/components/notifications/controller.notifications.js
@@ -1,22 +1,20 @@
 'use strict';
 calendar.controller( "Controller_Notifications", function( $scope, $state, $http, $stateParams, $view, Session ) {
-	console.log("user NOtifications " + Session.id_user);
   $http.get( 'http://localhost:3111/api/v1/notifications/' + Session.id_user )
     .success( function(response) {
       console.log( response );
-      $scope.Groups = response;
+      $scope.notifications = response;
     })
     .error( function() {
       console.log( "error getting notifications" );
     })
   ;
 
-  $scope.onClick_save = function() {
+  $scope.event_clicked_clearNotifications = function() {
     console.log( "clicked save" );
     var details = {
-      title: $scope.input_title,
       notifications: $scope.notifications,
-	  clearedNotifications: $socpe.cleared
+	  clearedNotifications: $scope.cleared
     };
 
     $http.post( 'http://localhost:3111/api/v1/events', details )
