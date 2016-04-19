6d370a700d20f93e3418a6def1d78d577a74cf3f
WarrenFerrell <warren.ferrell@colorado.edu>
2016-04-19 15:05:44 -0600

    add Find Events Near Me button that gets user's current location  (functionality not implimented yet)

--- api_server/credentials.js
+++ api_server/credentials.js
@@ -1,5 +1,5 @@
-var mongo_user = "****";
-var mongo_pass = "****";
+var mongo_user = "backend-node";
+var mongo_pass = "abcd123!@#";
 
 exports.mongo_user = mongo_user;
 exports.mongo_pass = mongo_pass;
